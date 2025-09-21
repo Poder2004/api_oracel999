260f451cefc16b63e5aa2864d6f36829afc991cf
poder <66011212129@gmail.com>
2025-09-21 16:11:51 +0700

    แก้

--- database/db.go
+++ database/db.go
@@ -1,13 +1,19 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// ฟังก์ชันสำหรับการเชื่อมต่อฐานข้อมูล
 func SetupDatabaseConnection() (*gorm.DB, error) {
-	dsn := "mb68_66011212129:px4uyNPZOfxE@tcp(202.28.34.203:3306)/mb68_66011212129?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		// fallback สำหรับ local
+		dsn = "mb68_66011212129:px4uyNPZOfxE@tcp(202.28.34.203:3306)/mb68_66011212129?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
